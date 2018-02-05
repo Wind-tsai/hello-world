607df92ef5cccdef7f30343871bd7cf4c07bbfb6
Wind-tsai <33213953+Wind-tsai@users.noreply.github.com>
2018-02-05 13:07:11 +0800

    hello_world

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello-world
+//
+//  Created by Wind on 2018/2/5.
+//  Copyright © 2018年 翟洋洋. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
